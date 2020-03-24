031dd155ca8f4ec3222428dd1ef3c71f92690e43
ZackJorquera <jorquerazack@gmail.com>
2020-03-24 15:18:25 -0600

    Removed redundant util files

--- rust-dns-name-resolution/build.rs
+++ rust-dns-name-resolution/build.rs
@@ -1,10 +1,12 @@
 extern crate cc;
 
 fn main() {
+    let c_file = "../PA3/util.c";
+    let lib_name = "dnsutil";
     // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo:rerun-if-changed=src/hello.c");
+    println!("cargorerun-if-changed={}", c_file);
     // Use the `cc` crate to build a C file and statically link it.
     cc::Build::new()
-        .file("src/util.c")
-        .compile("dnsutil");
+        .file(c_file)
+        .compile(lib_name);
 }
